3ab9994be6d613c690bac06df8e45f5161da2e5f
Richard <richardzehungho@gmail.com>
2022-08-20 09:41:28 -0700

    Add function to playRound to loop game

--- rps.js
+++ rps.js
@@ -1,12 +1,12 @@
-console.log("Hello World!");
+console.log("Let's play: Rock Paper Scissors!");
 
 
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 
-const player = playerSelection();
-const computer = getComputerChoice();
+let player = playerSelection();
+let computer = getComputerChoice();
 
 let computerScore = 0;
 let playerScore = 0;
@@ -44,30 +44,65 @@ function playRound(player, computer) {
     if (player === computer) {
         const draw = "This is a draw";
         // console.log(draw);
-        return draw;
+        return console.log("Draw");
     } // rock beats scissors 
     else if (player === 0 && computer === 2){
         console.log("player beats computer");
+        return playerScore += 1;
     } //vice versa
     else if (player === 2 && computer === 0){
         console.log("computer beats player")
+        return computerScore += 1;
     }
     //paper beats rock
     else if (player === 1 && computer === 0){
         console.log("player beats computer");
+        return playerScore += 1;
     }
     // vice versa
     else if (player === 0 && computer === 1){
         console.log("computer beats player");
+        return computerScore += 1;
     }
     //scissors beats paper
     else if (player === 2 && computer === 1){
         console.log("player beats computer");
+        return playerScore += 1;
     }
     // vice versa
     else if (player === 1 && computer === 2){
         console.log("computer beats player");
+        return computerScore += 1;
     }
 }
 
-console.log(playRound(player,computer));
+
+function game(){
+    // call playround
+    //set a win counter
+    // who ever has more wins in 5 loops in the winner = winner
+    //loop the playround function 5 times
+    for (let index = 1; index < 5; index++) {
+        playRound(player, computer);
+        console.log(playerScore);
+        console.log(computerScore);
+        player = playerSelection();
+        computer = getComputerChoice();
+    }
+
+    
+
+    if (playerScore > computerScore){
+        console.log("Player wins!");
+        console.log("finale score player: " + playerScore + " computer :" + computerScore);
+    }else if (computerScore > playerScore){
+        console.log("computer wins!");
+        console.log("finale score player: " + playerScore + " computer :" + computerScore);
+    }else {
+        console.log("It's a tie!")
+        console.log("finale score player: " + playerScore + " computer :" + computerScore);
+    }
+}
+// console.log(playRound(player,computer));
+
+game();
